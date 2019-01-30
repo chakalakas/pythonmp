da1cc806e5d05420d6fa8ca89a10c47ccc85a4de
Sergey Shilin <shilinshtein@gmail.com>
2019-01-30 16:09:20 -0500

    improved sortings tests

--- pythonmp/tests/test_sortings.py
+++ pythonmp/tests/test_sortings.py
@@ -7,12 +7,12 @@ except NameError:
 
 import time
 import random
+import numpy as np
+import pandas as pd
 from tqdm import tqdm
 
 from pythonmp import sort as pymp_sort
-
-NUM_ARRAYS_TO_GENERATE = 100
-ARRAY_SIZE = 10000
+sortings = [sorted, pymp_sort]
 
 
 def listsEqual(l1, l2):
@@ -26,56 +26,186 @@ def listsEqual(l1, l2):
     return True
 
 
-def test_sortings_1d_uints():
-    time_python = 0
-    time_pymp = 0
-
-    for i in tqdm(xrange(NUM_ARRAYS_TO_GENERATE)):
-        l = [random.randint(0, 1000) for _ in xrange(ARRAY_SIZE)]
-
-        time_python_start = time.time()
-        l_sorted_python = sorted(l)
-        time_python += time.time() - time_python_start
-
-        time_pymp_start = time.time()
-        l_sorted_our = pymp_sort(l)
-        time_pymp = time.time() - time_pymp_start
-
-        assert listsEqual(l_sorted_our, l_sorted_python)
-
-    time_python = time_python / NUM_ARRAYS_TO_GENERATE
-    time_pymp = time_pymp / NUM_ARRAYS_TO_GENERATE
-
-    max_t = max(time_pymp, time_python)
-    min_t = min(time_pymp, time_python)
-    ratio = (max_t - min_t) / min_t * 100
-
-    print ("PyMP sort is CORRECT")
-    print ("PyMP sort is in average {}% {}".format(ratio,
-        "faster" if time_pymp < time_python else "slower"))
-    print ("Python sort time: {}".format(time_python))
-    print ("PyMP   sort time: {}".format(time_pymp))
+def print_results(test_name, times):
+    print ()
+    print ("PASSED: ", test_name)
+    results = pd.DataFrame(
+        [times],
+        columns=[str(m.__name__) for m in sortings],
+        dtype=np.float)
+    print (results.to_string(index=False))
 
 
-def test_sortings_1d_int():
-    pass
+def generate_random_array(container, size, dtype):
+    min_int = -100
+    max_int = 100
+
+    if dtype not in [int, float]:
+        raise TypeError("Invalid data type: {}".format(dtype.__name__))
+
+    if container == list:
+        if dtype == int:
+            return [random.randint(min_int, max_int) for _ in xrange(size)]
+        else:
+            return [random.uniform(min_int, max_int) for _ in xrange(size)]
+    elif container == tuple:
+        if dtype == int:
+            return tuple(random.randint(min_int, max_int) for _ in xrange(size))
+        else:
+            return tuple(random.uniform(min_int, max_int) for _ in xrange(size))
+    elif container == np.ndarray:
+        if dtype == int:
+            return np.random.randint(min_int, max_int, size=size)
+        else:
+            return np.random.rand(size) * 2 - 1
+    else:
+        raise TypeError("Cannot generate an array of type {}".format(
+            container.__name__))
 
 
-def test_sortings_1d_float32():
-    pass
+def generate_test(test, iterations, container, size, dtype):
+    times = []
+
+    for i in xrange(len(sortings)):
+
+        time_run = 0
+        for _ in xrange(iterations):
+            arr = generate_random_array(container, size, dtype)
+            arr_sorted_true = sorted(arr)
 
 
-def test_sortings_1d_float64():
-    pass
+            current_sort = sortings[i]
+            time_start = time.time()
+            arr_sorted = current_sort(arr)
+            time_run += time.time() - time_start
+
+            # Check correctness
+            assert listsEqual(arr_sorted, arr_sorted_true)
+
+        times.append(time_run / iterations)
+
+    print_results(test, times)
 
 
-def test_sortings_1d_string():
-    pass
+def test_list_int_10():
+    name = "Container: list, size: 10, dtype: int"
+    size = 10
+    dtype = int
+    iterations = 1000
+    container = list
+    generate_test(name, iterations, container, size, dtype)
+
+
+def test_list_int_100():
+    name = "Container: list, size: 100, dtype: int"
+    size = 100
+    dtype = int
+    iterations = 300
+    container = list
+    generate_test(name, iterations, container, size, dtype)
+
+
+def test_list_int_1000():
+    name = "Container: list, size: 1000, dtype: int"
+    size = 1000
+    dtype = int
+    iterations = 100
+    container = list
+    generate_test(name, iterations, container, size, dtype)
+
+
+def test_list_int_1e4():
+    name = "Container: list, size: 1e4, dtype: int"
+    size = int(1e4)
+    dtype = int
+    iterations = 20
+    container = list
+    generate_test(name, iterations, container, size, dtype)
+
+
+def test_list_int_1e5():
+    name = "Container: list, size: 1e5, dtype: int"
+    size = int(1e5)
+    dtype = int
+    iterations = 10
+    container = list
+    generate_test(name, iterations, container, size, dtype)
+
+
+def test_list_int_1e6():
+    name = "Container: list, size: 1e6, dtype: int"
+    size = int(1e6)
+    dtype = int
+    iterations = 5
+    container = list
+    generate_test(name, iterations, container, size, dtype)
+
+
+def test_list_int_1e7():
+    name = "Container: list, size: 1e7, dtype: int"
+    size = int(1e7)
+    dtype = int
+    iterations = 1
+    container = list
+    generate_test(name, iterations, container, size, dtype)
+
+
+def test_list_float_1e5():
+    name = "Container: list, size: 1e5, dtype: float"
+    size = int(1e5)
+    dtype = float
+    iterations = 10
+    container = list
+    generate_test(name, iterations, container, size, dtype)
+
+
+def test_tuple_int_1e5():
+    name = "Container: tuple, size: 1e5, dtype: int"
+    size = int(1e5)
+    dtype = int
+    iterations = 10
+    container = tuple
+    generate_test(name, iterations, container, size, dtype)
+
+
+def test_tuple_float_1e5():
+    name = "Container: tuple, size: 1e5, dtype: float"
+    size = int(1e5)
+    dtype = float
+    iterations = 10
+    container = tuple
+    generate_test(name, iterations, container, size, dtype)
+
+
+def test_ndarray_int_1e5():
+    name = "Container: ndarray, size: 1e5, dtype: int"
+    size = int(1e5)
+    dtype = int
+    iterations = 10
+    container = np.ndarray
+    generate_test(name, iterations, container, size, dtype)
+
+
+def test_ndarray_float_1e5():
+    name = "Container: ndarray, size: 1e5, dtype: float"
+    size = int(1e5)
+    dtype = float
+    iterations = 10
+    container = np.ndarray
+    generate_test(name, iterations, container, size, dtype)
 
 
 if __name__ == "__main__":
-    test_sortings_1d_uints()
-    test_sortings_1d_int()
-    test_sortings_1d_float32()
-    test_sortings_1d_float64()
-    test_sortings_1d_string()
+    test_list_int_10()
+    test_list_int_100()
+    test_list_int_1000()
+    test_list_int_1e4()
+
+    test_list_int_1e5()
+    test_list_float_1e5()
+
+    test_tuple_int_1e5()
+    test_tuple_float_1e5()
+
+    test_ndarray_int_1e5()
+    test_ndarray_float_1e5()
